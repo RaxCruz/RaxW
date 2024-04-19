afa368cacf5ae0ec3277ab345e4a879787e100ad
RaxCruz <peter030910@gmail.com>
2024-04-19 12:34:09 +0800

    new feature:delay minus

--- components/ui/reveal_sole.tsx
+++ components/ui/reveal_sole.tsx
@@ -27,7 +27,7 @@ export default function RevealSole({
         }}
         initial="hidden"
         animate={mainControls}
-        transition={{ duration: 0.5, delay: 0.35 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
       >
         {children}
       </motion.div>
